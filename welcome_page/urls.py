27bd846b196e054f19d8bea952d99890bb4b7fff
Yuki Iida <yuki.iida@tier4.jp>
2017-12-18 14:20:37 +0900

    update layout

--- welcome_page/urls.py
+++ welcome_page/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'autoware_web'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='mypage'),
+    url(r'^$', views.index, name='login'),
+    url(r'^$', views.index, name='logout'),
     url(r'^docker/$', views.docker_install, name='docker'),
 ]
