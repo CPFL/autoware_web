36243c180960209c632d49029e5681c5d03d2a5d
Yuki Iida <yuki.iida@tier4.jp>
2018-02-01 11:05:07 +0900

    add sample data donwload function

--- welcome_page/urls.py
+++ welcome_page/urls.py
@@ -5,13 +5,16 @@ from . import views
 app_name = 'autoware_web'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.index, name='mypage'),
-    url(r'^$', views.index, name='login'),
-    url(r'^$', views.index, name='logout'),
+    # url(r'^$', views.index, name='mypage'),
+    # url(r'^$', views.index, name='login'),
+    # url(r'^$', views.index, name='logout'),
     url(r'^docker_x86/$', views.docker_x86_install, name='docker_x86'),
     url(r'^docker_px2/$', views.docker_px2_install, name='docker_px2'),
     #url(r'^development/$', views.development, name='development'),
     url(r'^join/$', views.join, name='join'),
-    url(r'^development/$', views.development, name='development'),
-    url(r'^development2/$', views.development2, name='development2'),
+    # url(r'^development/$', views.development, name='development'),
+    # url(r'^development2/$', views.development2, name='development2'),
+    url(r'^sample/sample_moriyama_data.tar.gz/$', views.sample_data1, name='sample_moriyama_data.tar.gz'),
+    url(r'^sample/my_launch.sh/$', views.sample_data2, name='my_launch.sh'),
+    url(r'^sample/sample_moriyama_150324.tar.gz/$', views.sample_data3, name='sample_moriyama_150324.tar.gz'),
 ]
--- welcome_page/views.py
+++ welcome_page/views.py
@@ -27,3 +27,12 @@ def docker_x86_install(request):
 def docker_px2_install(request):
     # return render(request, 'welcome_page/docker_px2_tier4.html')
     return redirect('https://github.com/CPFL/Autoware/wiki/Installation-by-Docker:-DRIVE-PX2')
+
+def sample_data1(request):
+    return redirect('https://autoware.blob.core.windows.net/sample/sample_moriyama_data.tar.gz')
+
+def sample_data2(request):
+    return redirect('https://autoware.blob.core.windows.net/sample/my_launch.sh')
+
+def sample_data3(request):
+    return redirect('https://autoware.blob.core.windows.net/sample/sample_moriyama_150324.tar.gz')
